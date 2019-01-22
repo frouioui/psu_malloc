e6989761f2aff74cf232b9dbbf1de44e2af6d37b
Florent Poinsard <florent.poinsard@epitech.eu>
2019-01-22 13:42:09 +0100

    Addition of the page creation

--- include/page.h
+++ include/page.h
@@ -8,12 +8,22 @@
 #ifndef PAGE_H_
 	#define PAGE_H_
 
+#include <stdlib.h>
+#include <unistd.h>
 #include "node.h"
 
 typedef struct page_s {
-    node_t *node;
+    node_t *node_allocated;
+    node_t *node_freed;
     size_t pagesize;
     struct page_s *next;
+    struct page_s *before;
 } page_t;
+
+const size_t DEFAULT_MULTIPLICATION_FACTOR = 32;
+
+page_t *new_page(size_t size);
+
+page_t *head;
 
 #endif /* !PAGE_H_ */
