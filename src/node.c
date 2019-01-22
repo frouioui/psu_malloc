76ab55885d439cb41f4b3485e6ba6ecbcd97727a
Cécile <cecile.cadoul@epitech.eu>
2019-01-22 13:44:12 +0100

    [ADD] - Adding my_sbrk

--- src/node.c
+++ src/node.c
@@ -12,11 +12,9 @@ const size_t addr_size = 8;
 const size_t bool_size = 8;
 const size_t size_t_size = 8;
 
-void push_node(node_t *head, size_t size, void *current_addr)
+void *my_sbrk(node_t *previous)
 {
-    size_t increment = size + addr_size * 2 + bool_size + size_t_size;
-    node_t *current = head;
-    node_t *new = NULL;
-
-    if (!current)
+    if (previous->data_addr == NULL)
+        return ((void*)previous);
+    return ((void *)(previous + sizeof(node_t) + previous->node_size));
 }
